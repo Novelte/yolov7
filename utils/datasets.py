2bc4520a4f5b67ba19035855eda4dcee1033e7bd
sam <sam>
2022-09-27 15:54:59 +0800

    debug

--- utils/datasets.py
+++ utils/datasets.py
@@ -644,6 +644,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
 
         img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
         img = np.ascontiguousarray(img)
+        print("img.shape:", img.shape)
         return torch.from_numpy(img.copy()), labels_out, self.img_files[index], shapes
 
     @staticmethod
