63c3f71330b9c1a72489e7cc9d444a480b06a101
royCheung-223 <cwhvhhh@gmail.com>
2023-02-20 02:15:14 +0000

    log to debug

--- seg/utils/segment/dataloaders.py
+++ seg/utils/segment/dataloaders.py
@@ -58,7 +58,7 @@ def create_dataloader(path,
             downsample_ratio=mask_downsample_ratio,
             overlap=overlap_mask,
             ch=ch)
-
+    print('after LoadImagesAndLabelsAndMasks')
     batch_size = min(batch_size, len(dataset))
     nd = torch.cuda.device_count()  # number of CUDA devices
     nw = min([os.cpu_count() // max(nd, 1), batch_size if batch_size > 1 else 0, workers])  # number of workers
@@ -66,6 +66,7 @@ def create_dataloader(path,
     loader = DataLoader if image_weights else InfiniteDataLoader  # only DataLoader allows for attribute updates
     # generator = torch.Generator()
     # generator.manual_seed(0)
+    print('before return loader')
     return loader(
         dataset,
         batch_size=batch_size,
